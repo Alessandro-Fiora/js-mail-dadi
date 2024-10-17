216c6c06069a1b6cf562bfdc367e34ea2514f367
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-17 14:28:32 +0200

    mail pseudocode

---
+++ mail/script.js
@@ -0,0 +1,17 @@
+// Mail
+// Chiedi all'utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull'esito del controllo.
+
+// ! DATI
+// * HO un ARRAY di persone che possono accedere
+// * CHIEDO la mail all'utente
+
+// ! ELABORAZIONE
+// * SE la MAIL DELL'UTENTE è presente nell'Array di  persone che possono accedere
+// * AGGIORNO OUTPUTMESSAGE con "il tuo account esiste"
+// * ALTRIMENTI
+// * AGGIORNO OUTPUTMESSAGE con "NON esiste nessun account abbinato a questa mail"
+
+// ! OUTPUT
+// * STAMPO OUTPUTMESSAGE
